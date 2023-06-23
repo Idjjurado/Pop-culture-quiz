953624709423d495abde57e1ff99e888c0f56233
idjjurado <ismael@eduphoria.net>
2023-06-22 19:18:16 -0600

    styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -388,3 +388,53 @@ function closeScoreModal() {
 function closeOptionModal() {
     document.getElementById('option-modal').style.display = "none"
 }
+
+var counter = {
+    square: txt => {
+    let cell = document.createElement("div");
+    cell.className = `cell ${txt}`;
+    cell.innerHTML = `<div class="digits">0</div>
+    <div class="text">${txt}</div>`;
+    return cell;
+}
+,
+
+attach: instance => {
+instance.target.className = "countdown";
+if (instance.remain >= 60) {
+instance.target.appendChild(counter.square("mins"));
+instance.mins = instance.target.querySelector(".mins .digits");
+}
+instance.target.appendChild(counter.square("secs"));
+instance.secs = instance.target.querySelector(".secs .digits");
+
+instance.timer = setInterval(() => {counter.ticker(instance);}, 1000);
+},
+
+ticker: instance => {
+instance.remain--;
+    if (instance.remain <= 0) { clearInterval(instance.timer); instance.remain=0; 
+        if (typeof instance.after=="function" ) {
+            instance.after();
+        } 
+    }     
+    let secs=instance.remain; 
+
+    let mins=Math.floor(secs / 60); 
+    secs -=mins * 60; instance.secs.innerHTML=secs; 
+    
+    if (instance.mins !==undefined) {
+        instance.mins.innerHTML=mins;
+        } 
+    }, 
+
+toSecs: till=> {
+    till = Math.floor(till / 1000);
+    let remain = till - Math.floor(Date.now() / 1000);
+    return remain < 0 ? 0 : remain; } }; window.onload=()=> {
+        counter.attach({
+        target: document.getElementById("demo"),
+        remain: 86500,
+        after: () => {alert("TIMER HAS ENDED!");} });
+
+        };
